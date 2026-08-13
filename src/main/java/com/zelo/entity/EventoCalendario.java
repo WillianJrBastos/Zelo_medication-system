5c51fb7c65604ef001934ac2d5ddd9026281dbda
WillianJrBastos <wjbastos1@gmail.com>
2026-08-13 11:27:40 -0300

    Entidade EventoCalendario feita

--- src/main/java/com/zelo/entity/EventoCalendario.java
+++ src/main/java/com/zelo/entity/EventoCalendario.java
@@ -1,4 +1,98 @@
 package com.zelo.entity;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class EventoCalendario {
+
+    private long id;
+    private String titulo;
+    private String descricao;
+    private LocalDateTime dataHora;
+    private String tipo;
+    private Usuario usuario;
+
+    public EventoCalendario() {
+    }
+
+    public EventoCalendario(long id, String titulo, String descricao, LocalDateTime dataHora, String tipo, Usuario usuario) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.dataHora = dataHora;
+        this.tipo = tipo;
+        this.usuario = usuario;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        EventoCalendario that = (EventoCalendario) o;
+        return id == that.id && Objects.equals(titulo, that.titulo) && Objects.equals(descricao, that.descricao) && Objects.equals(dataHora, that.dataHora) && Objects.equals(tipo, that.tipo) && Objects.equals(usuario, that.usuario);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, titulo, descricao, dataHora, tipo, usuario);
+    }
+
+    @Override
+    public String toString() {
+        return "EventoCalendario{" +
+                "id=" + id +
+                ", titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", dataHora=" + dataHora +
+                ", tipo='" + tipo + '\'' +
+                ", usuario=" + usuario +
+                '}';
+    }
 }
