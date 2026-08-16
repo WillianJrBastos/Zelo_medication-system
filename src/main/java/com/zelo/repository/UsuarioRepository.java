2c59daf90c690539d29c7c60ccea7abc03c2ac1c
WillianJrBastos <wjbastos1@gmail.com>
2026-08-16 13:47:24 -0300

    Finalizado o UsuarioRepository 2.0

--- src/main/java/com/zelo/repository/UsuarioRepository.java
+++ src/main/java/com/zelo/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.zelo.repository;
 import com.zelo.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String Email);
 }
